65d34dc935d3c915f9aba7abc92398e35cacc517
gitjavaee <14301164@bjtu.edu.cn>
2016-12-27 17:14:44 +0800

    hw

--- service/serviceImpl/PictureServiceImpl.java
+++ service/serviceImpl/PictureServiceImpl.java
@@ -1,35 +1,32 @@
 package cn.edu.bjtu.weibo.service.impl;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import cn.edu.bjtu.weibo.dao.ImgDAO;
+import cn.edu.bjtu.weibo.dao.impl.ImgDAOImpl;
 import cn.edu.bjtu.weibo.model.Picture;
 import cn.edu.bjtu.weibo.service.PictureService;
 
 public class PictureServiceImpl implements PictureService {
 
 	@Override
-	public Picture uploadPicture(MultipartFile multipartFile) {
-		Picture ap = new Picture();
-		if(!multipartFile.isEmpty()){
-			try {
-				InputStream ins = multipartFile.getInputStream();
-				byte[] b = new byte[ins.available()];
-				int len = 0;
-				while(len<ins.available()){
-					len+=ins.read(b, len, ins.available()-len);
-				}
-				String pictureContent = new String(b);
-				ap.setPicurl(pictureContent);
-			} catch (IOException e) {
-				// TODO 自动生成的 catch 块
-				e.printStackTrace();
-				ap.setPicurl("");
-			}
-		}
-		return ap;
+	public List<String> uploadPicture(MultipartFile multipartFile) {
+		// TODO 自动生成的方法存根
+		List<String> pictures = new ArrayList<String>();
+		pictures.add("23456");
+		return pictures;
 	}
 
+	@Override
+	public Picture getPicture(String picId) {
+		// TODO 自动生成的方法存根
+		ImgDAO aimg = new ImgDAOImpl();
+		Picture apic = new Picture();
+		apic.setPicurl(aimg.getimgThUrl(picId));
+		apic.setPicurlor(aimg.getimgOrUrl(picId));
+		return apic;
+	}
 }
